86ae3d15248a9c2c9a641cf79451b5068cedd9d8
David Navarro <david.navarro@mantech.com>
2023-06-15 12:20:57 -0500

    Added pcap folder ingest to arkime capture. Set read_from_disk to true to have it read pcaps off of file system instead of reading from interface.

--- deploy_arkime.sh
+++ deploy_arkime.sh
@@ -3,8 +3,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 pushd $SCRIPT_DIR > /dev/null
 
-helm uninstall arkime3
+helm uninstall arkime -n arkime
 helm package chart
-helm install arkime3 arkime3-viewer-2.0.0.tgz
+helm install arkime arkime3-viewer-2.0.0.tgz -n arkime
 
 popd > /dev/null
